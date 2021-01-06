53d706b2a82fe57e5228a8cfeb950e0e7c00ce3d
claxb <claxbrown@gmail.com>
2021-01-06 15:18:58 -0600

    color

--- scripts.js
+++ scripts.js
@@ -14,10 +14,9 @@ let currentMarker = 'X'
 
 // this "handleClick" function is called when a box is clicked. Here, "element" will hold the same value as "this" does in the HTML. 
 // "this" is a special word in JS but "element" could have been "thing" or "el" or whatever we wanted it to be as long as we use it again in the "console.log" statement
-const handleClick = (element) => {
-
-  // this uses the "log" method on the "console" to log out the element's id so we can see it with our human eyes
+const handleClick = (element) => { 
   console.log(`The element you clicked on has an id:  ${element.id}`)
+  
 
   // this next line prevents an X being changed to an O or an O being changed to an X by...
   //  checking to see if the square clicked has anything in it, if not continue
@@ -25,15 +24,6 @@ const handleClick = (element) => {
     addMarker(element.id)
   }
 }
-
-
-
-
-
-
-
-
-
 
 
 // this function places the "currentMarker" inside the HTML element that was clicked and calls the "changeMarker" function.
@@ -44,7 +34,7 @@ const addMarker = (id) => {
   console.log(`Therefore, a  "${currentMarker}"  should be placed in the square with the id:  ${id}`)
   
   // @TODO-2: Build a line of code that will set the innerHTML property of the element that was clicked to the "currentMarker"
-  
+  document.getElementById(id).innerHTML = currentMarker
   // @TODO-2.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // = currentMarker
   // .getElementById(id)
@@ -86,7 +76,7 @@ const resetBoard = () => {
   
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
-    
+      const squares = document.getElementsByTagName("TD")
   // @TODO-3.5: MIX & MATCH, You will need the following pieces of code to build that line:
   // squares
   // .getElementsByTagName("TD")
